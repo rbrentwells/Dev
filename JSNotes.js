5d135fd9cee9f8bd5777fbbb85f5bf6fdb6628d6
Shifty <rbrentwells@gmail.com>
2017-05-31 08:22:27 -0600

    Update JSNotes.js

--- JSNotes.js
+++ JSNotes.js
@@ -1,2 +1,32 @@
 # Dev
 DMJSNOTES
+
+>>>>>>>>>>>>>>>>>>>>>>>
+  #79 Chaining If Else Statements
+
+function testSize(num) {
+  // Only change code below this line
+  
+  if (num < 5) {
+    return "Tiny";
+  }else if (num < 10) {
+    return "Small";
+  }else if (num < 15) {
+    return "Medium";
+  }else if (num < 20) {
+    return "Large";
+  }else {
+    return "Huge";
+  }
+  
+  return "Change Me";
+  // Only change code above this line
+}
+
+// Change this value to test
+testSize(7);
+
+
+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+  #80
+
